1881eb82a03fe4ae3f1fa4ee19d65d0e70be9a25
mabolhasani <mohammad.abolhasany@gmail.com>
2021-01-04 17:02:57 +0330

    added class for check if myket product purcased

--- Myket/Myket/MyketApi.cs
+++ Myket/Myket/MyketApi.cs
@@ -1,8 +1,44 @@
-﻿using System;
+﻿using Myket.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Myket
 {
-    public class Class1
+    public class MyketApi
     {
+        private readonly Configuration _configuration;
+        private readonly HttpClient _client;
+        private const string BaseUrl = "https://developer.myket.ir/";
+        public MyketApi(Configuration configuration)
+        {
+            _configuration = configuration;
+            _client = new HttpClient();
+        }
+
+        public async Task<VerifyPurchaseResult> VerifyPurchase(string sku, string token)
+        {
+            _client.DefaultRequestHeaders.Add("X-Access-Token", _configuration.AccessToken);
+
+            var response = await _client.GetAsync(
+                $"{BaseUrl}/api/applications/" +
+                $"{_configuration.PackageName}/purchases/products/{sku}/tokens/" +
+                $"{token}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = JsonConvert.DeserializeObject<Result>(
+                    await response.Content.ReadAsStringAsync());
+
+                return new VerifyPurchaseResult(result);
+            }
+            else
+            {
+                var result = JsonConvert.DeserializeObject<Error>(
+                    await response.Content.ReadAsStringAsync());
+
+                return new VerifyPurchaseResult(result);
+            }
+        }
     }
 }
